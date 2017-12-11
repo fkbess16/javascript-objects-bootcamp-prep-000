d4caf50f890db14387de82620fbee929f1a8c486
Flannery Bessette <fkbess16@gmail.com>
2017-12-11 18:32:16 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { taylorswift :"are you ready for it?"};
-function updatePlaylist (playlist,artist,song){
-  return playlist['artist'] = "song";
+function updatePlaylist (playlist,artistName,songTitle){
+  return playlist['artistName'] = songTitle;
 }
