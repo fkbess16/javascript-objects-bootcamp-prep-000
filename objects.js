909d9f3704bb739c8b519129f687f95601c7b96a
Flannery Bessette <fkbess16@gmail.com>
2017-12-11 18:44:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { kanye :"Golddigger"};
+var playlist = { Kanye :"Gold Digger"};
 function updatePlaylist (playlist,artistName,songTitle){
   playlist[artistName] = songTitle;
   return playlist
